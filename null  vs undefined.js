002dc4b1523ff4b7b633f4dd68017052720277be
sakiramostary012 <sakiramostary97@gmail.com>
2020-03-12 22:05:12 +0600

    null vs undefined

---
+++ null  vs undefined.js
@@ -0,0 +1,32 @@
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1, num2){
+    console.log(num1 + num2)
+
+    return
+}
+
+const result = add(13, 83);
+console.log(result);
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const result = add(12);
+console.log(result);
+
+
+const premik = {name: "sakib", phone: 01988};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
