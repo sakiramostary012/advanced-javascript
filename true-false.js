4df279e2c371af0461bbd5c8e1a50ac204a84a17
sakiramostary012 <sakiramostary97@gmail.com>
2020-03-12 21:01:22 +0600

    true or false in js

---
+++ true-false.js
@@ -0,0 +1,25 @@
+//Falsy
+//0
+//""
+//undefined
+//null
+//NaN
+//false 
+
+//Truthy
+//"0"
+//" "
+//[]
+//{}
+//'false'
+
+//const name = "solaiman khan";
+
+let name = 12;
+
+if(name || name == 0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
